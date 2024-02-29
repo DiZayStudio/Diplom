622f6e6d817e2a389d14c85d625a7727488eb15a
Dmitry Zaytsev <97391679+DiZayStudio@users.noreply.github.com>
2024-02-29 20:48:35 +0300

    Delete out

--- out/build/x64-Debug/pqxx/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/libpq/test
-# Build directory: C:/Work/diplom/out/build/x64-Debug/pqxx/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(runner "C:/Work/diplom/out/build/x64-Debug/pqxx/test/runner.exe")
-set_tests_properties(runner PROPERTIES  _BACKTRACE_TRIPLES "C:/libpq/test/CMakeLists.txt;12;add_test;C:/libpq/test/CMakeLists.txt;0;")
